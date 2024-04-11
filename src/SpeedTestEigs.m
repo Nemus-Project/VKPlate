1c79449776e43c020ab0db36f3fa756a2251668d
AlexisMousseau <153088037+AlexisMousseau@users.noreply.github.com>
2024-04-11 16:40:21 +0200

    Made a speed test for the eigs function
    Catch Matthew !

--- src/SpeedTestEigs.m
+++ src/SpeedTestEigs.m
@@ -35,32 +35,50 @@ D       = E * Lz^3 / 12 / (1-nu^2) ;
 
 %%
 
-nbpts=1000;
+nbpts=100;
 modes=zeros(nbpts,1);
 
-nxny=zeros(nbpts,1);
+
  Nx=100;
  Ny=100;
  h=0.01;
+ ncalc=10;
+ 
 
- biHarm=bhmat(BCs,[Nx,Ny],h,Lz,E,nu,'diag');
 
+ %%
+nbpts=100;
+eig10t=zeros(nbpts,1);
+eig20t=zeros(nbpts,1);
+eig50t=zeros(nbpts,1);
+
+nxny=zeros(nbpts,1);
 for n=nbpts:-1:1
-    nmod(n)= n;
+    Nx=10*n;
+    Ny=1000;
+    h=0.01;
+    nxny(n)= Nx*Ny;
+    biHarm=bhmat(BCs,[Nx,Ny],h,Lz,E,nu,'diag');
+
     tic
-    [~,~] = eigs(biHarm,n,'smallestabs') ;
-    modes(n)=toc;
+    [~,~] = eigs(biHarm,10,'smallestabs') ;
+    eig10t(n)=toc
 
-    
+    tic
+    [~,~] = eigs(biHarm,20,'smallestabs') ;
+    eig20t(n)=toc
 
+    tic
+    [~,~] = eigs(biHarm,50,'smallestabs') ;
+    eig50t(n)=toc
     disp(n)
 
 
 end
 %%
 figure
-plot(nmod,modes,Linewidth=3)
-%legend("50 modes","100 modes","200 modes")
-xlabel("Number of modes computed")
+plot(nxny,eig10t,nxny,eig20t,nxny,eig50t,Linewidth=3)
+legend("10 modes","20 modes","50 modes")
+xlabel("Total Number of Grid Points")
 ylabel("Time (s)")
 set(gca,'Fontsize',20)
