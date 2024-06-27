16173b252a0510bb1dbca0ecf8b144f52fc40642
AlexisMousseau <153088037+AlexisMousseau@users.noreply.github.com>
2024-06-27 14:41:13 +0200

    Update Platecode1mode.m
    This code is now working due to a modification of the NL potential U

--- src/Platecode1mode.m
+++ src/Platecode1mode.m
@@ -16,6 +16,7 @@ y       =linspace(0,Ly,Ny+1);
 
 %--- mode reconstruction
 Nmodes=1;
+%Hv(1,1,1)=100;
 %%
 Id=1;
 
@@ -30,10 +31,10 @@ Ddiag=2*Dw;
 %%
 qm=zeros(Nmodes,1);
 
-q0=zeros(Nmodes,1);
+%q0=zeros(Nmodes,1);
 
-qmsv=zeros(Nmodes,1);
-q0sv=zeros(Nmodes,1);
+%qmsv=zeros(Nmodes,1);
+%q0sv=zeros(Nmodes,1);
 
 
 dUdq=zeros(Nmodes,1);
@@ -55,7 +56,6 @@ ETASV=zeros(Ts,Nmodes);
 
 eta=zeros(Nmodes,1);
 
-qh=zeros(Nmodes,Nmodes,Nmodes);
 %% Initial displacement
 qm(1)=1e-2;
 %%
@@ -69,6 +69,11 @@ Q(1,:)=q0;
 
 QSV(1,:)=q0sv;
 
+gamma=(E/(rho*2*zetafourth(1)))*Hv(1,1,1)^2;
+um1=qm(1);
+[s,c,d] = ellipj(sqrt(Om(1)^2+gamma*um1^2)*t,(gamma*um1^2)/(2*gamma*um1^2+2*Om(1)^2));    %Analytical solution
+uana=um1*c;
+
 %% Initialisation
 
 
@@ -80,10 +85,13 @@ etasv=eta;
 ETA(1,:)=eta;
 ETASV(1,:)=etasv;
 
-U=(1/(2*E*Lz))*zetafourth(1)*0.5*(eta.^2+eta.^2);   %to change with actual energy
+enerr=1/(rho*Lz);
+
+U=enerr*(1/(2*E*Lz))*zetafourth(1)*0.5*(eta.^2+eta.^2);   %to change with actual energy
 
 ep=1e-14;
 psim1=sqrt(2*U+ep);
+
 
 %%
 
@@ -104,11 +112,13 @@ for n = 2 : Ts
 
     ETASV(n,:)=etasv;
 
-    U=(1/(2*E*Lz))*zetafourth(1)*(eta.^2);
+    U=enerr*(1/(2*E*Lz))*zetafourth(1)*(eta.^2);
     
     dUdq=-eta*q0*Hv(1,1,1);
            
-    g=(1/sqrt(2*U+ep))*dUdq;
+    g=enerr*(1/sqrt(2*U+ep))*dUdq;
+
+    %g=enerr*(1/sqrt(2*U+ep))*(E*Lz/(2*zetafourth(1)))*Hv(1,1,1)^2*q0^3;
     
     Q(n,:)=q0;
 
@@ -119,7 +129,12 @@ for n = 2 : Ts
     
     %qs=(1/(1+(g^2*k^2)/4)) * ((2-Om(1)^2*k^2)*q0+(((g^2*k^2)/4)-1)*qm-k^2*g*psim1); %SAV but not exact
 
-    qssv=(2-k^2*Om(1)^2)*q0 - qm + ((Hv(1,1,1)*k^2)/(rho*Lz))*q0*eta ;%Stormer Verlet integrator
+    %qs=(1/(1+(g^2*k^2)/4)) * ((2-Om(1)^2*k^2)*q0+(((g^2*k^2)/4)-1)*qm-(1/(rho*Lz))*k^2*g*psim1); %SAV with scaling (energy not conserved)
+
+    qssv=(2-k^2*Om(1)^2)*q0sv - qmsv + ((Hv(1,1,1)*k^2)/(rho*Lz))*q0sv*etasv ;%Stormer Verlet integrator
+
+    %qssv=(2-k^2*Om(1)^2)*q0sv - qmsv + ((Hv(1,1,1)*k^2))*q0sv*etasv ;%Stormer Verlet integratorrescaled
+
 
     psi0=psim1+0.5*g*(qs-qm);
 
@@ -162,15 +177,15 @@ xlabel('Timestep')
 ylabel('Amplitude')
 set(gca,'FontSize',20)
 figure
-plot(t,Q(:,:),t,QSV(:,:),LineWidth=3)
-title('w_k SAV & Stormer Verlet')
+plot(t,Q(:,:),t,QSV,t,uana,LineWidth=3)
+title('w_k SAV ')
 xlabel('Times')
 ylabel('Amplitude')
 set(gca,'FontSize',20)
 
 
 %%
-err=(Q(:,:)-QSV(:,:));
+err=(Q-QSV);
 figure
 plot(err,LineWidth=3)
 title('Absolute error')
